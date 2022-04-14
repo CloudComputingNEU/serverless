fb6887d1004f5de73ec521f33be17eed6aaa92bc
SivaDheerajReddyNEU <obulam.s@northeastern.edu>
2022-04-14 02:31:08 -0400

    code changes

--- index.js
+++ index.js
@@ -1,32 +1,65 @@
 console.log('Loading function');
+const aws = require("aws-sdk");
+const ses = new aws.SES({});
+exports.handler = async function (event, context, callback) {
 
-exports.handler = function(event, context, callback) {
-
-    let message = event.Records[0].Sns.Message;
-    console.log('Message received from SNS:', message);
-
-    let arr = message.split("_");
-    console.log(arr);
-
-    
-    let fromAddress=arr[0];
-    let toAddress=arr[1];
-    let link=arr[2];
-      var params = {
-        Destination: {
-          ToAddresses: [toAddress],
+  let message = event.Records[0].Sns.Message;
+  console.log('Message received from SNS:', message);
+  let data = JSON.parse(message);
+  console.log(data);
+  let fromAddress = data.fromAddress;
+  let toAddress = data.toAddress;
+  let userName = data.userName;
+  let link = data.link;
+  let params = {
+    Destination: 
+    {
+      ToAddresses: [toAddress],
+    },
+    Message: 
+    {
+      Body: 
+      {
+        Html: 
+        {
+          Data:
+            ` <!DOCTYPE html>
+              <html>
+                <head>
+                  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                  <title>Account Verification</title>
+                  <style>
+                    body {
+                      background-color: #FFFFFF;
+                      padding: 10px;
+                      margin: 10px;
+                    }
+                  </style>
+                </head>
+                <body style="background-color: #FFFFFF; padding: 10px; margin: 10px;">
+                <article>
+                  <h1>
+                    Hi ${userName},
+                  </h1>
+                  <br>
+                  <h2>
+                    Welcome to ${data.domain},
+                  </h2>
+                  <p> Please user <a href=${link}>link</a> to verify your account.  </p>
+                  <br>
+                  <p> if you are unable to user the link, copy paste the below link in your browser</p>
+                  <p>${link}</p>
+                </article>
+                </body>
+              </html>`
         },
-        Message: {
-          Body: {
-            Text: { Data: "please click this link to verify- "+link },
-          },
-    
-          Subject: { Data: "Test Email" },
-        },
-        Source: fromAddress,
-      };
-     
-      await ses.sendEmail(params).promise();
-    callback(null, "Success");
+      },
+      Subject: { Data: `Account Verification - ${data.toAddress}` },
+    },
+    Source: fromAddress,
+  };
+
+  await ses.sendEmail(params).promise();
+  callback(null, "Success");
 
 };
---
+++ test.js
@@ -0,0 +1,9 @@
+let data={
+  "fromAddress":"no-reply@prod.dheerajreddy.me",
+  "toAddress":"obulam.dheeraj@gmail.com",
+  "userName":"Dheeraj",
+  "link":"http://localhost:8080/verifyEmail?",
+  "domain":"prod.dheerajreddy.me"
+}
+
+console.log(JSON.stringify(data));
